ca2d31d9b1d2d896348351085c49d8aa95937bbb
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2017-02-04 14:34:38 -0200

    Components de hello world

--- mini-ecommerce-webschool/src/App.vue
+++ mini-ecommerce-webschool/src/App.vue
@@ -1,8 +1,18 @@
 <template>
+  <div>
+    <CcHello></CcHello>
+  </div>
 </template>
 
 <script>
+import CcHello from './components/Hello.vue'
+
 export default {
+  name: 'WebschoolComponent',
+
+  components: {
+    CcHello
+  }
 }
 </script>
 
--- mini-ecommerce-webschool/src/components/Hello.vue
+++ mini-ecommerce-webschool/src/components/Hello.vue
@@ -1,10 +1,25 @@
 <template>
+  <div>
+    <h1>Olá coleguinhas da Webshool!</h1>
+
+    <CcIntro></CcIntro>
+  </div>
 </template>
 
 <script>
+import CcIntro from './Intro.vue'
+
 export default {
+  components: {
+    CcIntro
+  }
 }
 </script>
 
-<style>
+<style scoped>
+h1{
+  font-size: 50px;
+  font-weight: bold;
+  color: #f00;
+}
 </style>
